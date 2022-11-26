783a7a24a49bc027b405331e235f6f1ba6f94dd7
Varvara568-34684-68-5 <varvara.payateleva@yandex.ru>
2022-11-26 10:31:22 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Captcha1
+{
+    class Class1
+    {
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Captcha1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string text = String.Empty;
+        private Bitmap CreateImage(int Width, int Height)
+        {
+            Random rnd = new Random();
+
+            //Создадим изображение
+            Bitmap result = new Bitmap(Width, Height);
+
+            //Вычислим позицию текста
+            int Xpos = rnd.Next(0, Width - 50);
+            int Ypos = rnd.Next(15, Height - 15);
+
+            //Добавим различные цвета
+            Brush[] colors = { Brushes.Black,
+                     Brushes.Red,
+                     Brushes.RoyalBlue,
+                     Brushes.Green };
+
+            //Укажем где рисовать
+            Graphics g = Graphics.FromImage((Image)result);
+
+            //Пусть фон картинки будет серым
+            g.Clear(Color.Gray);
+
+            //Сгенерируем текст
+            text = String.Empty;
+            string ALF = "1234567890QWERTYUIOPASDFGHJKLZXCVBNM";
+            for (int i = 0; i < 5; ++i)
+                text += ALF[rnd.Next(ALF.Length)];
+
+            //Нарисуем сгенирируемый текст
+            g.DrawString(text,
+                         new Font("Arial", 15),
+                         colors[rnd.Next(colors.Length)],
+                         new PointF(Xpos, Ypos));
+
+            //Добавим немного помех
+            /////Линии из углов
+            g.DrawLine(Pens.Black,
+                       new Point(0, 0),
+                       new Point(Width - 1, Height - 1));
+            g.DrawLine(Pens.Black,
+                       new Point(0, Height - 1),
+                       new Point(Width - 1, 0));
+            ////Белые точки
+            for (int i = 0; i < Width; ++i)
+                for (int j = 0; j < Height; ++j)
+                    if (rnd.Next() % 20 == 0)
+                        result.SetPixel(i, j, Color.White);
+
+            return result;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        
+            {
+                pictureBox1.Image = this.CreateImage(pictureBox1.Width, pictureBox1.Height);
+            }
+           
+         
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = this.CreateImage(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == this.text)
+                MessageBox.Show("Верно!");
+            else
+                MessageBox.Show("Ошибка!");
+        }
+    }
+}
